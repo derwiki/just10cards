1844821b4fdd26cbc3347a3ce032218978913eff
Adam Derewecki <dereweck@gmail.com>
2011-09-29 21:53:04 -0700

    Switch from MySQL to SQLite

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Just10cards::Application.config.secret_token = '5b50f5255eeda1830cb15f9792b976a562222f7ea488bdf06c03d270d2250d950e70151b3ecc369e9af713e7f82a3e6fb52c7ab8277551df093b2b2f375976bb'
+Just10cards::Application.config.secret_token = '9d77083c70574e172d609bf3d45a0ff6819ec69aae03e63845466b4c74615f3c78ba044ad238e5de855aece99f9a9fb0a24d6597091b083dccdfa52d43af2438'
