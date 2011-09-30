40e7054e1f5fd078276740cf3cffe30f0eee3e5a
Adam Derewecki <derwiki@elcalafate.local>
2011-09-29 21:46:27 -0700

    initial rails commit by your homie glenn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Just10cards::Application.config.secret_token = '5b50f5255eeda1830cb15f9792b976a562222f7ea488bdf06c03d270d2250d950e70151b3ecc369e9af713e7f82a3e6fb52c7ab8277551df093b2b2f375976bb'
