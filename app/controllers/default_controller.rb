d34f11f17d33d463fb9f60653457b96df8b16857
Adam Derewecki <derewecki@gmail.com>
2011-10-02 13:33:56 -0700

    Constantize magic numbers

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,14 +1,23 @@
+CARD_HEIGHT = 7 # number of rows per card
+CARD_WIDTH = 40 # width in chars of each card
+CARD_COLS = 2 # number of cards per row in layout
+CARD_ROWS = 5 # number of rows of cards in layout
+
 class DefaultController < ApplicationController
   def show
     render
   end
 
   def cards
-    @horizontal_divider = (['-' * 40] * 2).join('+')
-    @card = params[:card].split("\r\n").map {|line| line.center(40)}
-    while @card.size < 7 do
-      @card << ' ' * 40
+    @card = params[:card].split
+    while @card.size < CARD_HEIGHT do
+      @card << ' '
     end
-    @tagline = (["just10cards.com".rjust(40)] * 2).join('|')
+    @card = @card.map do |line|
+      ([line.center(CARD_WIDTH)] * CARD_COLS).join('|')
+    end
+
+    @horizontal_divider = (['-' * CARD_WIDTH] * CARD_COLS).join('+')
+    @tagline = (["just10cards.com".rjust(CARD_WIDTH)] * CARD_COLS).join('|')
   end
 end
