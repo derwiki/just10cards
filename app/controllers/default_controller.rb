8fefa7a465d722d63020a2b63c615e3ccca30868
Adam Derewecki <derewecki@gmail.com>
2011-10-02 12:18:08 -0700

    Buffer to 7 rows for the card if <7 rows inputted

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -4,7 +4,10 @@ class DefaultController < ApplicationController
   end
 
   def cards
-    @card = params[:card].split("\r\n").map {|line| line.center(40)}
     @horizontal_divider = "+#{(['-' * 40] * 2).join('+')}+\n"
+    @card = params[:card].split("\r\n").map {|line| line.center(40)}
+    while @card.size < 7 do
+      @card << ' ' * 40
+    end
   end
 end
