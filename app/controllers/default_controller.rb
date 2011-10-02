8e01b0130c04408c993a93010be443c3d8a232af
Adam Derewecki <derewecki@gmail.com>
2011-10-02 13:19:04 -0700

    Move layout manipulation code to controller

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -4,10 +4,11 @@ class DefaultController < ApplicationController
   end
 
   def cards
-    @horizontal_divider = "+#{(['-' * 40] * 2).join('+')}+\n"
+    @horizontal_divider = (['-' * 40] * 2).join('+')
     @card = params[:card].split("\r\n").map {|line| line.center(40)}
     while @card.size < 7 do
       @card << ' ' * 40
     end
+    @tagline = (["just10cards.com".rjust(40)] * 2).join('|')
   end
 end
